87846844ff8a09e64c7e70a26b386bf4dde44f8c
Gregory Guths <guthsgregory@gmail.com>
2025-03-28 15:02:49 -0400

    TiposVariaveis

---
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -0,0 +1,58 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+
+        //Tipo de dados	  Descrição
+        //byte	          Armazena números inteiros de -128 a 127
+        //short	          Armazena números inteiros de -32.768 a 32.767
+        //int	          Armazena números inteiros de -2.147.483.648 a 2.147.483.647
+        //long	          Armazena números inteiros de -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+        //float	          Armazena números fracionários. Suficiente para armazenar de 6 a 7 dígitos decimais//É preciso o uso do sufixo f ou F no final do número
+        //double	      Armazena números fracionários. Suficiente para armazenar de 15 a 16 dígitos decimais////É preciso o uso do sufixo d ou D no final do número
+        //String	      Armazena uma sequência de caracteres. É uma classe, não um tipo primitivo
+        //boolean	      Armazena valores verdadeiros ou falsos
+        //char	          Armazena um único caractere/letra ou valores ASCII
+
+        // Definindo uma variável do tipo inteiro
+        int idade = 30;
+        System.out.println("Idade: " + idade);
+
+        // Definindo uma variável do tipo double
+        double altura = 1.75;
+        System.out.println("Altura: " + altura);
+
+        // Definindo uma variável do tipo char
+        char letra = 'A';
+        System.out.println("Letra: " + letra);
+
+        // Definindo uma variável do tipo boolean
+        boolean ativo = true;
+        System.out.println("Ativo: " + ativo);
+
+        // Definindo uma variável do tipo String
+        String nome = "João";
+        System.out.println("Nome: " + nome);
+
+        // Definindo uma variável do tipo int
+        int x = 5;
+        int y = 6;
+        int z = 50;
+        System.out.println(x + y + z);
+        // Ou podemos fazer assim:
+        int k= 1, o = 4, j = 70;
+        System.out.println(k * o * j);
+
+        // Definindo uma variável do tipo boolean
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+
+        
+
+
+
+
+
+        
+    }
+}
