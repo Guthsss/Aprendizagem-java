f1f80c2f82492705c498866b7159c788c41de5fd
Gregory Guths <guthsgregory@gmail.com>
2025-03-28 15:22:45 -0400

    Operadores de comparação

---
+++ Operadores/src/OperadoresComparação.java
@@ -0,0 +1,24 @@
+public class OperadoresComparação {
+    public static void main(String[] args) {
+        
+        //      Operador	Nome	            Exemplo	
+        //      ==	        Igual a	            x == y	
+        //      !=	        Não igual	        x != y	
+        //      >	        Maior que	        x > y	
+        //      <	        Menos de	        x < y	
+        //      >=	        Maior ou igual a	x >= y	
+        //      <=	        Menor ou igual a	x <= y
+
+        // Comparação de dois números inteiros
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+        System.out.println("a > b: " + (a > b));   // false
+        System.out.println("a < b: " + (a < b));   // true
+
+        
+    }
+}
