089fb7f8d3e6b713eb2bf3b7ab466f45d0f9c1d6
Gregory Guths <guthsgregory@gmail.com>
2025-05-05 21:09:50 -0400

    Adicionado programa MetodoOperadores para realizar operações de multiplicação e divisão com variáveis static e entrada de valores via Scanner.

--- Metodos/src/MetodoOperadores.java
+++ Metodos/src/MetodoOperadores.java
@@ -1,22 +1,41 @@
+import java.util.Scanner;
+
 public class MetodoOperadores {
-    
-    static double operadores (double a, double b, double c, double d) {
 
-        return ((a * b) / (c - d));
+    static double a;
+    static double b;
+
+    static double multiplicacao() {
+
+        return (a * b);
     }
-    static int operadores1 (int x, int y, int z) {
 
-        return ((x) *  (y + z));
+    static double divisao() {
+
+        return (a / b);
     }
 
     public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.print("Atriabua um valor para A: ");
+            a = scanner.nextDouble();
+
+            System.out.print("Atribua um valor a B: ");
+            b = scanner.nextDouble();
+        }
+
+        double resultado1 = multiplicacao();
+        System.out.println("O resultado da multiplicação é: " + resultado1);
+
+
+        if (b > 0) {
+        double resultado2 = divisao();
+        System.out.println("O resultado da multiplicação é: " + resultado2);
+        } else {
+            System.out.println("O divisor não pode ser zero ou número negativo");
+        }
         
-        double resuldado = operadores(2, 30, 100, 60);
-
-        System.out.println("O resultado da multiplicação é " + resuldado);
-
-        int resultado1 = operadores1(20, 2, 3);
-
-        System.out.println("O resultado da soma é " + resultado1);
     }
 }
