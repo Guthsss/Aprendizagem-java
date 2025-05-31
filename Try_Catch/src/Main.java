e8879eb2454fdbfdad20a9e202e97e927835058b
Gregory Guths <guthsgregory@gmail.com>
2025-05-31 16:03:17 -0300

    Add Main class demonstrating various try-catch examples and integrate Try_Catch folder in project settings

--- Try_Catch/src/Main.java
+++ Try_Catch/src/Main.java
@@ -57,3 +57,13 @@ public class Main {
         }
     }
 }
+
+/*Tratamento de exceções
+E quando inevitavelmente ocorrer uma exceção? Como nós desenvolvedores podemos ajustar o nosso algoritmo para amenizar o ocorrido?
+
+A instrução try permite que você defina um bloco de código para ser testado quanto a erros enquanto está sendo executado.
+
+A instrução catch permite definir um bloco de código a ser executado, caso ocorra um erro no bloco try.
+
+A instrução finally permite definir um bloco de código a ser executado independente de ocorrer um erro ou não. As palavras-chave try e catch vêm em pares:
+ */
