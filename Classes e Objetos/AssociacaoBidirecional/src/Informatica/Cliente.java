3eecc9106911fa3aaf6f43fce8dfbcf921f52e65
=Gregory Guths <=guthsgregory@gmail.com>
2025-11-08 18:18:17 -0300

    Extendendo classes para incluir funcionalidades adicionais e criando fluxos principais no método `Main`.

--- Classes e Objetos/AssociacaoBidirecional/src/Informatica/Cliente.java
+++ Classes e Objetos/AssociacaoBidirecional/src/Informatica/Cliente.java
@@ -11,4 +11,7 @@ public class Cliente {
     public void consultarPedido(Pedido pedido) {
         System.out.println("Número do pedido: " + pedido.getNumero() + "Data: " + pedido.getData() + "Status: " + pedido.getStatus() + "Técnico: " + pedido.getTecnico());
     }
+    public void exibirDados() {
+        System.out.println("Nome do cliente: " + this.nome + "Endereço: " + this.endereco);
+    }
 }
